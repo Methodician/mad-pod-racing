3a62e358c84bd42c91b401a15a7cd4a629e40a01
Methodician <methodician@gmail.com>
2023-02-09 08:48:07 -0800

    Working system with more types and more smarts

--- CodeRyale.ts
+++ CodeRyale.ts
@@ -1,20 +1,32 @@
-type BarracksTypes = "ARCHER" | "KNIGHT";
+// NOTE random idea - build tower at center and always
+// try to grow it unless I need to build more barracks
 
-let nextBarracksType: BarracksTypes = "KNIGHT";
+type BarracksTypes = "ARCHER" | "KNIGHT" | "GIANT"; // should add "GIANT" here
+type BuildingTypes = BarracksTypes | "TOWER";
+
+let nextBuildingType: BuildingTypes = "KNIGHT";
 let gold = 0;
 const centerpoint = () => new Coords(1920 / 2, 1000 / 2);
 
 let units: Unit[] = []; // could track enemy units to decide what to build
 
 const sites: Record<number, Site> = {};
-const ownedBarracks = {} as Record<number, BarracksTypes>;
+const ownedBuildings = {} as Record<number, BuildingTypes>;
 
-// ARCHER / KNIGHT
-const targetBuildRatio = 1 / 2;
+// ARCHER / KNIGHT / GIANT
+const targetTrainingRatio = {
+  KNIGHT: 0.35,
+  ARCHER: 0.4,
+  GIANT: 0.25,
+};
+const targetKnightBarracks = 1;
+const targetArcherBarracks = 1;
+const targetGiantBarracks = 1;
 
 let ownedBarracksCounts: Record<BarracksTypes, number> = {
   ARCHER: 0,
   KNIGHT: 0,
+  GIANT: 0,
 };
 
 class Unit {
@@ -43,32 +55,6 @@ class Queen extends Unit {
     super(owner, location, health, -1);
   }
 
-  takeTurn = () => {
-    const _ownedBarracksCounts = Object.values(ownedBarracks).reduce(
-      (acc, type) => {
-        acc[type] = acc[type] + 1;
-        return acc;
-      },
-      {
-        ARCHER: 0,
-        KNIGHT: 0,
-      } as Record<BarracksTypes, number>
-    );
-
-    ownedBarracksCounts = _ownedBarracksCounts;
-    const nearbySite = nearestToMe();
-    const builtRatio =
-      _ownedBarracksCounts.ARCHER / _ownedBarracksCounts.KNIGHT;
-
-    if (builtRatio < targetBuildRatio) {
-      nextBarracksType = "ARCHER";
-    } else {
-      nextBarracksType = "KNIGHT";
-    }
-
-    this.buildBarracks(nearbySite.id, nextBarracksType);
-  };
-
   attackSite = (site: Site) => {
     this.moveToLocation(site.location.x, site.location.y);
   };
@@ -80,6 +66,8 @@ class Queen extends Unit {
 
   buildBarracks = (siteId: number, type: string) =>
     console.log(`BUILD ${siteId} BARRACKS-${type}`);
+
+  buildTower = (siteId: number) => console.log(`BUILD ${siteId} TOWER`);
 }
 
 class Site {
@@ -104,14 +92,14 @@ class Site {
       // a new Barracks was built
       if (owner === 0) {
         // it was mine
-        ownedBarracks[this.id] = nextBarracksType;
+        ownedBuildings[this.id] = nextBuildingType;
       }
     }
     if (this.type === 2 && type !== 2) {
       // a Barracks was destroyed
       if (this.owner === 0) {
         // it was mine
-        delete ownedBarracks[this.id];
+        delete ownedBuildings[this.id];
       }
     }
 
@@ -180,7 +168,7 @@ const nearestToCenter = () => {
 
 const knightBarracksNearestEnemy = () => {
   const knightBarracks = Object.entries(sites)
-    .filter((s) => ownedBarracks[s[0]] === "KNIGHT")
+    .filter((s) => ownedBuildings[s[0]] === "KNIGHT")
     .map((s) => s[1]);
 
   const nearestToEnemy = knightBarracks.sort(
@@ -194,7 +182,7 @@ const knightBarracksNearestEnemy = () => {
 
 const archerBarracksNearestQueen = () => {
   const archerBarracks = Object.entries(sites)
-    .filter((s) => ownedBarracks[s[0]] === "ARCHER")
+    .filter((s) => ownedBuildings[s[0]] === "ARCHER")
     .map((s) => s[1]);
 
   const nearestToSelf = archerBarracks.sort(
@@ -204,6 +192,107 @@ const archerBarracksNearestQueen = () => {
   );
 
   return nearestToSelf[0];
+};
+
+const giantBarracksNearestTower = () => {
+  const giantBarracks = Object.entries(sites)
+    .filter((s) => ownedBuildings[s[0]] === "GIANT")
+    .map((s) => s[1]);
+
+  const towers = Object.values(sites).filter((s) => s.type === 1);
+
+  const towerNearestQueen = towers.sort(
+    (a, b) =>
+      myQueen.location.distanceBetween(a.location) -
+      myQueen.location.distanceBetween(b.location)
+  )[0];
+
+  const giantBarracksNearestTower = giantBarracks.sort(
+    (a, b) =>
+      towerNearestQueen.location.distanceBetween(a.location) -
+      towerNearestQueen.location.distanceBetween(b.location)
+  );
+  return giantBarracksNearestTower[0];
+};
+
+// GAME LOGIC
+
+const queenTurn = () => {
+  ownedBarracksCounts = Object.values(ownedBuildings).reduce(
+    (acc, type) => {
+      acc[type] = acc[type] + 1;
+      return acc;
+    },
+    {
+      ARCHER: 0,
+      KNIGHT: 0,
+      GIANT: 0,
+    } as Record<BarracksTypes, number>
+  );
+
+  const nearbySite = nearestToMe();
+
+  console.error(JSON.stringify(ownedBarracksCounts, null, 2));
+
+  if (ownedBarracksCounts.ARCHER < targetArcherBarracks) {
+    nextBuildingType = "ARCHER";
+    myQueen.buildBarracks(nearbySite.id, nextBuildingType);
+  } else if (ownedBarracksCounts.KNIGHT < targetKnightBarracks) {
+    nextBuildingType = "KNIGHT";
+    myQueen.buildBarracks(nearbySite.id, nextBuildingType);
+  } else if (ownedBarracksCounts.GIANT < targetGiantBarracks) {
+    nextBuildingType = "GIANT";
+    myQueen.buildBarracks(nearbySite.id, nextBuildingType);
+  } else {
+    nextBuildingType = "TOWER";
+    myQueen.buildTower(nearbySite.id);
+  }
+
+  //   myQueen.buildBarracks(nearbySite.id, nextBuildingType);
+};
+
+const trainTurn = () => {
+  const myUnits = units.filter((u) => u.isMine());
+  const currentKnightsPercentage =
+    myUnits.filter((u) => u.unitType === 0).length / myUnits.length;
+  const currentArchersPercentage =
+    myUnits.filter((u) => u.unitType === 1).length / myUnits.length;
+  const currentGiantsPercentage =
+    myUnits.filter((u) => u.unitType === 2).length / myUnits.length;
+
+  console.error(
+    "knight/archer/giant",
+    currentKnightsPercentage,
+    currentArchersPercentage,
+    currentGiantsPercentage
+  );
+
+  // train units based on ration vs target ratio
+  if (currentKnightsPercentage < targetTrainingRatio.KNIGHT) {
+    // find nearest KNIGHT barracks to self
+    const nearestToSelf = knightBarracksNearestEnemy();
+    if (!!nearestToSelf) {
+      console.log(`TRAIN ${nearestToSelf.id}`);
+    } else {
+      console.log("TRAIN");
+    }
+  } else if (currentArchersPercentage < targetTrainingRatio.ARCHER) {
+    // find nearest ARCHER barracks to enemy
+    const nearestToEnemy = archerBarracksNearestQueen();
+    if (!!nearestToEnemy) {
+      console.log(`TRAIN ${nearestToEnemy.id}`);
+    } else {
+      console.log("TRAIN");
+    }
+  } else if (currentGiantsPercentage < targetTrainingRatio.GIANT) {
+    // find nearest GIANT barracks to enemy
+    const nearestToEnemy = giantBarracksNearestTower();
+    if (!!nearestToEnemy) {
+      console.log(`TRAIN ${nearestToEnemy.id}`);
+    } else {
+      console.log("TRAIN");
+    }
+  }
 };
 
 // READ INIT STUFF
@@ -220,7 +309,7 @@ for (let i = 0; i < numSites; i++) {
 // game loop
 while (true) {
   units = [];
-  // READ TURN STUFF
+  // READ INIT STUFF
   var inputs: string[] = readline().split(" ");
   const inputGold: number = parseInt(inputs[0]);
   const touchedSite: number = parseInt(inputs[1]); // -1 if none
@@ -230,12 +319,11 @@ while (true) {
     const siteId: number = parseInt(inputs[0]);
     const ignore1: number = parseInt(inputs[1]); // used in future leagues
     const ignore2: number = parseInt(inputs[2]); // used in future leagues
-    const structureType: number = parseInt(inputs[3]); // -1 = No structure, 2 = Barracks
+    const structureType: number = parseInt(inputs[3]); // -1 = No structure, 1 = Tower, 2 = Barracks
     const owner: number = parseInt(inputs[4]); // -1 = No structure, 0 = Friendly, 1 = Enemy
     const param1: number = parseInt(inputs[5]);
     const param2: number = parseInt(inputs[6]);
     sites[siteId].update(structureType, owner, param1, param2);
-    // console.error(sites[siteId].loggable());
   }
   const numUnits: number = parseInt(readline());
   for (let i = 0; i < numUnits; i++) {
@@ -243,7 +331,7 @@ while (true) {
     const x: number = parseInt(inputs[0]);
     const y: number = parseInt(inputs[1]);
     const owner: number = parseInt(inputs[2]);
-    const unitType: number = parseInt(inputs[3]); // -1 = QUEEN, 0 = KNIGHT, 1 = ARCHER
+    const unitType: number = parseInt(inputs[3]); // -1 = QUEEN, 0 = KNIGHT, 1 = ARCHER, 2 = GIANT
     const health: number = parseInt(inputs[4]);
     if (owner == 0 && unitType == -1) {
       if (myQueen.location.x == 0 && myQueen.location.y == 0) {
@@ -262,38 +350,14 @@ while (true) {
     }
   }
 
+  // GAME CODE
   // Write an action using console.log()
   // To debug: console.error('Debug messages...');
-
   // First line: A valid queen action
   // Second line: A set of training instructions
-  myQueen.takeTurn();
 
-  const myUnits = units.filter((u) => u.isMine());
-  const currentUnitsRatio =
-    myUnits.filter((u) => u.unitType === 1).length /
-    myUnits.filter((u) => u.unitType === 0).length;
-
-  console.error(currentUnitsRatio);
-
-  // train units based on ration vs target ratio
-  if (currentUnitsRatio < targetBuildRatio) {
-    // find nearest ARCHER barracks to self
-    const nearestToSelf = archerBarracksNearestQueen();
-    if (!!nearestToSelf) {
-      console.log(`TRAIN ${nearestToSelf.id}`);
-    } else {
-      console.log("TRAIN");
-    }
-  } else {
-    // find nearest KNIGHT barracks to enemy
-    const nearestToEnemy = knightBarracksNearestEnemy();
-    if (!!nearestToEnemy) {
-      console.log(`TRAIN ${nearestToEnemy.id}`);
-    } else {
-      console.log("TRAIN");
-    }
-  }
+  queenTurn();
+  trainTurn();
 }
 
 // MAKE VSCODE HAPPY
